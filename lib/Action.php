7d5739c8738f39f981028f7a3d6648294c83963c
Honghao Liang <fcrosfly@gmail.com>
2013-01-22 21:52:05 +0800

    #26 -1 提供了一个借口判断一个用户是不是关注了另一个用户 ```php Action::isLiked($user, $target_user) ```

--- lib/Action.php
+++ lib/Action.php
@@ -11,10 +11,17 @@ class Action extends Mysql{
 	public static function isFav($user, $video) {
 		$finder = new self;
 		$finder->type = self::TYPE_FAV;
-		$finder->userid = $visitor->id;
+		$finder->userid = $user->id;
 		$finder->target = $video->id;
 		return $finder->count() > 0 ;
 	}
+
+	public static function isLiked($user, $target_user) {
+		$finder = new self;
+		$finder->type = self::TYPE_LIKE_USER;
+		$finder->userid = $user->id;
+		$finder->target = $target_user->id;
+		return $finder->count() > 0 ;	}
 }
 
 ?>
