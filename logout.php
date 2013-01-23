8009519c21d445f4ca40a4921f5ed5a410c3ac17
Gavin Foo <fuxiaopang@msn.com>
2013-01-24 04:07:13 +0800

    # 31 -2

--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
 <?
 	include "include/init.php";
 	unset($_SESSION['user_id']);
-	header("LOCATION:" . $_SERVER['HTTP_REFERER'] ?: '/'). "&bye";
+	echo "<script>window.location = '" . ($_SERVER['HTTP_REFERER'] ?: '/') . "?bye'</script>";
 ?>
