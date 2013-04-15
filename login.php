dcf6a5e7e8e49deae8f62625c40e41a4b83ece84
Honghao Liang <fcrosfly@gmail.com>
2013-04-15 21:32:19 +0800

    清理未commit的代码

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ ob_start();
 $email = $_POST['email'];
 $password = $_POST['password'];
 if (Visitor::login($email, $password)) {
-	echo "<script>window.location = '" . ($_SERVER['HTTP_REFERER'] ?: '/') . "?welcome'</script>";
+	echo "<script>window.location = '" . ($_SERVER['HTTP_REFERER'] ? : '/') . "?welcome'</script>";
 } else {
-	echo "<script>window.location = '" . ($_SERVER['HTTP_REFERER'] ?: '/') . "?err'</script>";
+	echo "<script>window.location = '" . ($_SERVER['HTTP_REFERER'] ? : '/') . "?err'</script>";
 }
--- logout.php
+++ logout.php
@@ -1,5 +1,4 @@
-<?
+<?php
 	include "include/init.php";
 	unset($_SESSION['user_id']);
 	echo "<script>window.location = '" . ($_SERVER['HTTP_REFERER'] ?: '/') . "?bye'</script>";
-?>
