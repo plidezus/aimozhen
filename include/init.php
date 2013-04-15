8eecb3b477b985bc5daa52dae8c898daec42e868
Honghao Liang <fcrosfly@gmail.com>
2013-04-15 21:33:11 +0800

    把data和lib分开

--- include/init.php
+++ include/init.php
@@ -1,12 +1,23 @@
 <?php
 
 $pagename = "index" ;$headname = "index" ;
-$sitename = "艾墨镇" ;
-$sitedesc = "艾墨镇，分享视频，创造梦想" ;
+$siteoption = new Siteoption(3);$siteurl = $siteoption->value ;
+$siteoption = new Siteoption(4);$sitename = $siteoption->value ;
+$siteoption = new Siteoption(5);$sitetitle = $siteoption->value ;
+$siteoption = new Siteoption(6);$sitedescription = $siteoption->value ;
+$siteoption = new Siteoption(7);$admin_email = $siteoption->value ;
+
 session_start();
 function __autoload($class) {
 	$class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-	require_once __DIR__ . '/../lib/' . $class . '.php';
+	$dir_array = array('lib', 'data');
+	foreach ($dir_array as $each_dir) {
+		$file_name = __DIR__ . "/../{$each_dir}/" . $class . '.php';
+		if (is_file($file_name)) {
+			require_once $file_name;
+			break;
+		}
+	}
 }
 $visitor = Visitor::user();
 define('HTDOCS_DIR', __DIR__ . '/..');
